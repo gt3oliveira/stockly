6ea026c98e1892b0f96b6b20f4d2c874d020ecc9
gt3oliveira <gt3oliveira@gmail.com>
2025-10-03 15:33:35 -0300

    feat: add table product

---
+++ app/products/_compoments/table-columns.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Circle } from "lucide-react";
+
+export const productTableColumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Produto",
+  },
+  {
+    accessorKey: "price",
+    header: "Valor unitário",
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: (row) => {
+      const product = row.row.original;
+      const label =
+        product.status === "IN_STOCK" ? "Em estoque" : "Sem estoque";
+      return (
+        <Badge
+          variant={product.status === "IN_STOCK" ? "default" : "destructive"}
+        >
+          <Circle className="mr-1 fill-current" />
+          {label}
+        </Badge>
+      );
+    },
+  },
+];
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { db } from "@/lib/prisma";
+import { PlusIcon } from "lucide-react";
+import { productTableColumns } from "./_compoments/table-columns";
 
-export default function ProductsPage() {
-  return <div>ProductsPage</div>;
+export default async function ProductsPage() {
+  const products = await db.product.findMany({});
+
+  return (
+    <div className="m-6 w-full space-y-8 rounded-lg bg-white p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de produtos
+          </span>
+          <h2 className="text-xl font-semibold">Produtos</h2>
+        </div>
+        <Button>
+          <PlusIcon />
+          Novo produto
+        </Button>
+      </div>
+      <DataTable columns={productTableColumns} data={products} />
+    </div>
+  );
 }
