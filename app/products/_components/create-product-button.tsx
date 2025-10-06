99efb262385d558fcbdea32f2bf984dd455ee69b
gt3oliveira <gt3oliveira@gmail.com>
2025-10-06 15:36:20 -0300

    refactor: add UpsertProductDialogContent component

--- app/products/_components/create-product-button.tsx
+++ app/products/_components/create-product-button.tsx
@@ -1,36 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { useProductForm } from "@/data/create-product";
-import { Loader2Icon, PlusIcon } from "lucide-react";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { PlusIcon } from "lucide-react";
 import { useState } from "react";
-import { NumericFormat } from "react-number-format";
+import { UpsertProductDialogContent } from "./upsert-dialog-content";
 
 export const CreateProductButton = () => {
   const [dialogIsOpen, setDialogIsOpen] = useState(false);
-  const { form, onSubmit } = useProductForm({
-    onSuccess: () => setDialogIsOpen(false),
-    onError: () => setDialogIsOpen(false),
-  });
 
   return (
     <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
@@ -40,96 +17,7 @@ export const CreateProductButton = () => {
           Novo produto
         </Button>
       </DialogTrigger>
-      <DialogContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <DialogHeader>
-              <DialogTitle>Criar produto</DialogTitle>
-              <DialogDescription>
-                Insira as informações do produto no formulário abaixo.
-              </DialogDescription>
-            </DialogHeader>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Nome</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Nome do produto" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Preço</FormLabel>
-                  <FormControl>
-                    <NumericFormat
-                      decimalScale={2}
-                      fixedDecimalScale={true}
-                      thousandSeparator="."
-                      decimalSeparator=","
-                      prefix="R$ "
-                      allowNegative={false}
-                      customInput={Input}
-                      placeholder="Preço do produto"
-                      onValueChange={(value) =>
-                        field.onChange(value.floatValue)
-                      }
-                      {...field}
-                      onChange={() => {}}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="stock"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Quantidade em estoque</FormLabel>
-                  <FormControl>
-                    <NumericFormat
-                      decimalScale={1}
-                      allowNegative={false}
-                      customInput={Input}
-                      placeholder="Quantidade de produtos em estoque"
-                      onValueChange={(value) =>
-                        field.onChange(value.floatValue)
-                      }
-                      {...field}
-                      onChange={() => {}}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant={"secondary"} type="reset">
-                  Cancelar
-                </Button>
-              </DialogClose>
-              <Button type="submit" disabled={form.formState.isSubmitting}>
-                {form.formState.isSubmitting && (
-                  <Loader2Icon className="animate-spin" />
-                )}
-                Salvar
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
+      <UpsertProductDialogContent setIsOpen={setDialogIsOpen} />
     </Dialog>
   );
 };
---
+++ app/products/_components/upsert-dialog-content.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useProductForm } from "@/data/create-product";
+import { Loader2Icon } from "lucide-react";
+import { NumericFormat } from "react-number-format";
+import { toast } from "sonner";
+
+interface UpsertProductDialogContentProps {
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+export const UpsertProductDialogContent = ({
+  setIsOpen,
+}: UpsertProductDialogContentProps) => {
+  const { form, onSubmit } = useProductForm({
+    onSuccess: () => {
+      setIsOpen(false);
+      toast.success("Produto criado com sucesso!");
+    },
+    onError: () => {
+      setIsOpen(false);
+      toast.error("Ocorreu um erro ao criar o produto.");
+    },
+  });
+
+  return (
+    <DialogContent>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <DialogHeader>
+            <DialogTitle>Criar produto</DialogTitle>
+            <DialogDescription>
+              Insira as informações do produto no formulário abaixo.
+            </DialogDescription>
+          </DialogHeader>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nome do produto" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Preço</FormLabel>
+                <FormControl>
+                  <NumericFormat
+                    decimalScale={2}
+                    fixedDecimalScale={true}
+                    thousandSeparator="."
+                    decimalSeparator=","
+                    prefix="R$ "
+                    allowNegative={false}
+                    customInput={Input}
+                    placeholder="Preço do produto"
+                    onValueChange={(value) => field.onChange(value.floatValue)}
+                    {...field}
+                    onChange={() => {}}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="stock"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Quantidade em estoque</FormLabel>
+                <FormControl>
+                  <NumericFormat
+                    decimalScale={1}
+                    allowNegative={false}
+                    customInput={Input}
+                    placeholder="Quantidade de produtos em estoque"
+                    onValueChange={(value) => field.onChange(value.floatValue)}
+                    {...field}
+                    onChange={() => {}}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant={"secondary"} type="reset">
+                Cancelar
+              </Button>
+            </DialogClose>
+            <Button type="submit" disabled={form.formState.isSubmitting}>
+              {form.formState.isSubmitting && (
+                <Loader2Icon className="animate-spin" />
+              )}
+              Salvar
+            </Button>
+          </DialogFooter>
+        </form>
+      </Form>
+    </DialogContent>
+  );
+};
